9fb6463784101e4f45c09dcc2b0e454ee7230e4d
HamdiNur <hamdoonur26@gmail.com>
2024-10-24 21:27:35 +0300

    importing

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import BookDetails from './components/BookDetails/BookDetails';
+import BookList from './components/BookList/BookList';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  <BrowserRouter>
  <Routes>
+ <Route path= "/" element={<Home/>}/>
+ <Route path= "about" element={<About/>}/>
+ <Route path= "book" element={<BookList/>}/>
+ <Route path= "/book/:id" element={<BookDetails/>}/>
+
+
 
  </Routes>
  </BrowserRouter>
