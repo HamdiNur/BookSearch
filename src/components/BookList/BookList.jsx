fdeb0b1b8d46acdcc3d9f1365895e5a1902085dd
HamdiNur <hamdoonur26@gmail.com>
2026-05-24 14:28:48 +0300

    Build book list and book card rendering system

--- src/components/BookList/BookList.jsx
+++ src/components/BookList/BookList.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
+import { useGlobalContext } from '../../context';
+import Loading from "../Loader/Loader";
+import Book from '../BookList/Book'
+import coverImg from "../../images/cover_not_found.jpg";
+import "./BookList.css";
 
 const BookList = () => {
+  const { books, loading, resultTitle } = useGlobalContext();
+
+  const booksWithCovers = books.map((singleBook) => {
+    return {
+      ...singleBook,
+      id: singleBook.id.replace("/works/", ""),
+      cover_img: singleBook.cover
+        ? singleBook.cover
+        : coverImg
+    };
+  });
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  console.log(booksWithCovers);
+
   return (
-    <div>
-      
-    </div>
+    <section className='booklist'>
+      <div className='container'>
+        <h2>{resultTitle}</h2>
+
+<div className='booklist-content grid'>
+  {booksWithCovers.map((item) => (
+    <Book
+      key={item.id}
+      book={item}
+    />
+  ))}
+</div>
+      </div>
+    </section>
   );
-}
+};
 
 export default BookList;
