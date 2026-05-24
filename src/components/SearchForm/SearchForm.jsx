d9c45cd15b425c42d16ab27473e983612ec33c94
HamdiNur <hamdoonur26@gmail.com>
2026-05-24 14:26:35 +0300

    Implement search form with navigation and validation

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,19 +1,51 @@
-import React from 'react';
+import React,{useRef,useEffect} from 'react';
 import "./SearchForm.css";
 import { FaSearch } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
+import { useGlobalContext } from '../../context';
 
 const SearchForm = () => {
+  const { setSearchTerm, setResultTitle } = useGlobalContext();
+
+  const searchText = useRef('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    searchText.current.focus();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let tempsearchTerm = searchText.current.value.trim();
+
+    if ((tempsearchTerm.replace(/[ ]+/g, "")).length === 0) {
+      setSearchTerm('the lost world');
+      setResultTitle("Please enter something...");
+    } else {
+      setSearchTerm(tempsearchTerm);
+    }
+
+    navigate("/book");
+  };
+
   return (
     <div className='search-form'>
       <div className="container">
         <div className="search-form-content">
-          <form  className="search-form">
+          <form className="search-form" onSubmit={handleSubmit}>
             <div className="search-form-elem flex flex-sb bg-white">
-               <input type="text" className='form-control' placeholder='The Lost World ...'/>
-               <button type='submit' className="flex flex-c">
-                <FaSearch  className='text-purple' size={32} />
-               </button>
+              <input
+                type="text"
+                className='form-control'
+                placeholder='The Lost World ...'
+                ref={searchText}
+              />
+
+              <button type='submit' className="flex flex-c">
+                <FaSearch className='text-purple' size={32}/>
+              </button>
+
             </div>
           </form>
         </div>
