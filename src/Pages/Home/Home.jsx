efd782d3d372b43884c77b028e68b1167e66ee98
HamdiNur <hamdoonur26@gmail.com>
2026-05-25 14:17:32 +0300

    Clean up Home page and remove redundant Header usage

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import Header from '../../components/Header/Header';
-import { Outlet } from 'react-router-dom';
+import BookList from '../../components/BookList/BookList';
 
 const Home = () => {
   return (
-<main>
-  <Header/>
-  <Outlet/>
-</main>
+    <main>
+      <BookList/>
+    </main>
   );
 }
 
