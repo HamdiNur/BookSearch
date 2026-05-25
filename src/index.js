780e5bfa33035a4218daaeaddb603a00454de1f0
HamdiNur <hamdoonur26@gmail.com>
2026-05-25 14:18:22 +0300

    Refactor routing with Layout component and nested routes

--- src/index.js
+++ src/index.js
@@ -7,18 +7,23 @@ import About from './Pages/About/About';
 import BookDetails from './components/BookDetails/BookDetails';
 import BookList from './components/BookList/BookList';
 import { AppProvider } from './context';
+import Layout from './Pages/Layout/Layout';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <AppProvider>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="book" element={<BookList />} />
-        <Route path="/book/:id" element={<BookDetails />} />
-      </Routes>
+<Routes>
+  <Route path="/" element={<Layout />}>
+
+    <Route index element={<Home />} />
+    <Route path="about" element={<About />} />
+    <Route path="book" element={<BookList />} />
+    <Route path="book/:id" element={<BookDetails />} />
+
+  </Route>
+</Routes>
     </BrowserRouter>
   </AppProvider>
 );
